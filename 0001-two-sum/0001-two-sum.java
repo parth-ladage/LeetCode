5a258ae3e2a3695a126401da15dcb109d5163cfd
Parth Ladage <ladage.parth.p@gmail.com>
2026-07-29 19:47:20 +0530

    Time: 2 ms (99.31%), Space: 47.1 MB (40.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> seen = new HashMap<>();
-        int[] result = new int[2];
+        Map<Integer, Integer> seen = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             int comp = target - nums[i];
             if(seen.containsKey(comp)){
-                result[0] = seen.get(comp);
-                result[1] = i;
-            } else {
-                seen.put(nums[i], i);
+                return new int[]{ seen.get(comp), i};
             }
+            seen.put(nums[i], i);
+            
         }
-        return result;
+        return new int[]{ -1, -1};
     }
 }
