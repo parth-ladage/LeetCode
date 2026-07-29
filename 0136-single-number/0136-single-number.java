a773e8d2ec0b98ead4d3defea3035f8811aec095
Parth Ladage <ladage.parth.p@gmail.com>
2026-07-29 19:24:14 +0530

    Time: 1 ms (99.96%), Space: 46.9 MB (46.17%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> visited = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(visited.containsKey(nums[i])){
-                visited.put(nums[i], visited.get(nums[i]) + 1);
-            } else {
-                visited.put(nums[i], 1);
-            }
-        }
         int result = 0;
-        for(int key : visited.keySet()){
-            if(visited.get(key)==1){
-                result = key;
-            }
+        // HashMap<Integer, Integer> visited = new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     if(visited.containsKey(nums[i])){
+        //         visited.put(nums[i], visited.get(nums[i]) + 1);
+        //     } else {
+        //         visited.put(nums[i], 1);
+        //     }
+        // }
+        // for(int key : visited.keySet()){
+        //     if(visited.get(key)==1){
+        //         result = key;
+        //     }
+        // }
+
+        for(int num: nums){
+            result ^= num;
         }
 
         return result;
